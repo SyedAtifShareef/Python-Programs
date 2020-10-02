7c83ffde8eb67599d9d4d248d679539019b4f6cf
SyedAtifShareef <160418735103@mjcollege.ac.in>
2020-10-02 16:53:04 +0530

    Update P69_ReverseWords.py
    hi sir,
    I found your program very useful thanks a lot for sharing. I have added comments explaining your code.
    once again thanks a lot for sharing on the open-source networks.

--- Programs/P69_ReverseWords.py
+++ Programs/P69_ReverseWords.py
@@ -2,10 +2,15 @@
 
 # Python program to reverse the words
 
-userInput = input()
+userInput = input("Enter the word that needs to be reversed:")
+#the above function display the string and takes a string value.
+
 userInput = userInput.split()
+#.split() is an inbuild function that splits the given word to sub-letters.
 
 print(' '.join(userInput[::-1]))
+# the above line take the user defined function namely userInput and print the index from the last character to the first character 
+# the join function in the 11th line is used to join the reversed word with suceeding word with a space.
 
 # OUTPUT:
 # Computer Science
